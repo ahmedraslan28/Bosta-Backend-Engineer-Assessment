b5d3a658338f29eb1e25e42e7d8b59bfd09264f4
ahmedraslan28 <ahmedraslan28@gmail.com>
2025-08-02 15:12:52 +0300

    handle undefined routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,44 @@
 const express = require('express');
-const borrowerRoutes = require('./routes/borrower.routes');
-const errorHandler = require('./middlewares/error.middleware');
-const bookRoutes = require('./routes/book.routes');
-const borrowRoutes = require('./routes/borrow.routes')
-const reportRoutes = require('./routes/report.routes')
-const app = express();
 
+const borrowerRoutes = require('./routes/borrower.routes');
+const bookRoutes = require('./routes/book.routes');
+const borrowRoutes = require('./routes/borrow.routes');
+const reportRoutes = require('./routes/report.routes');
+
+const errorHandler = require('./middlewares/error.middleware');
+
+const app = express();
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'Library API is running',
+    endpoints: {
+      borrowers: '/api/borrowers',
+      books: '/api/books',
+      borrows: '/api/borrows',
+      reports: '/api/reports'
+    }
+  });
+});
 
 app.use('/api/borrowers', borrowerRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/borrows', borrowRoutes);
 app.use('/api/reports', reportRoutes);
 
-app.use(errorHandler);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Library API is running' });
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.originalUrl} not found`,
+    error: {
+      code: 'ROUTE_NOT_FOUND',
+      method: req.method,
+      path: req.originalUrl
+    }
+  });
 });
+
+app.use(errorHandler);
 
 module.exports = app;
